d11b6a667973a82173ce352a938afac186adad9a
Mary Ann Velar <mas.velar.01@gmail.com>
2023-09-09 17:13:55 +0800

    adjusted text size

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -6,7 +6,7 @@ const Hello = () => {
             <div className='grid grid-cols-1 content-center customMD:grid-cols-2'>
                 <div className='flex flex-col mb-4 mr-10 customMD:self-center'>
                     <div className='flex flex-row items-center'>
-                        <span className="font-chakraPetch lg:text-[20px] mr-5">HELLO. I AM </span>
+                        <span className="font-chakraPetch lg:text-[1.25rem] mr-5">HELLO. I AM </span>
                         <hr className="flex-grow sm:flex-shrink border-1 border-primary" />
                     </div>
                     <span className='text-accent font-chakraPetch font-semibold text-[3.052rem] lg:text-[3.815rem] leading-tight mb-2'> Mary Ann </span>
